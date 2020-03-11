ec592c989950a061db2bd88bc7f1fa6685238079
Nguyễn Minh Thư <thunm@sendo.vn>
2020-03-11 10:07:44 +0700

    Add Swagger

--- TTBackEndApi/Startup.cs
+++ TTBackEndApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace TTBackEndApi
 {
@@ -26,6 +27,18 @@ namespace TTBackEndApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //Add the swagger
+            services.AddSwaggerGen(c=>
+            {
+                c.SwaggerDoc(name: "v1", new OpenApiInfo { 
+                    Title = "My Api", 
+                    Version = "v1",
+                    Contact = new OpenApiContact() { Email="minhthu2511@gmail.com",Name="ThuNM",Url=new Uri("http://abcd.com") },
+                    Description = "Test v",
+                });
+            });
+            //End Add the swagger
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +48,14 @@ namespace TTBackEndApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c=>
+            {
+                c.SwaggerEndpoint(url: "/swagger/v1/swagger.json", name: "My API V1");
+            });
+
 
             app.UseHttpsRedirection();
 
