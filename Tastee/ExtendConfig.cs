e6c026d5b2cd15c79713b022a88d2143ee1ed909
Nguyễn Minh Thư <minhthu2511@gmail.com>
2020-09-22 21:58:51 +0700

    Update Project

---
+++ Tastee/ExtendConfig.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tastee.Models.SqlDataContext;
+using Tastee.Services;
+using URF.Core.Abstractions;
+using URF.Core.Abstractions.Trackable;
+using URF.Core.EF;
+using URF.Core.EF.Trackable;
+
+namespace Tastee
+{
+    public static class ExtendConfig
+    {
+        const string CONNECTION_STRING_NAME = "DbConnectString";
+        public static IServiceCollection InsideConfigServices(this IServiceCollection services, IConfiguration Configuration)
+        {
+            //services.AddDbContext<TTContext>(options =>
+            //options.UseNpgsql(Configuration.GetSection(CONNECTION_STRING_NAME).Value));
+
+            services.AddDbContext<TasteeContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("TasteeConnection")));
+
+            services.AddScoped<DbContext, TasteeContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<ITrackableRepository<Operators>, TrackableRepository<Operators>>();
+            services.AddScoped<ITTService<Operators>, TTService<Operators>>();
+
+            services.AddScoped<ITrackableRepository<Roles>, TrackableRepository<Roles>>();
+            services.AddScoped<ITTService<Roles>, TTService<Roles>>();
+
+            services.AddScoped<ITrackableRepository<OperatorRoles>, TrackableRepository<OperatorRoles>>();
+            services.AddScoped<ITTService<OperatorRoles>, TTService<OperatorRoles>>();
+
+            services.AddScoped<ITrackableRepository<Users>, TrackableRepository<Users>>();
+            services.AddScoped<ITTService<Users>, TTService<Users>>();
+
+            services.AddScoped<ITrackableRepository<Brands>, TrackableRepository<Brands>>();
+            services.AddScoped<ITTService<Brands>, TTService<Brands>>();
+
+            return services;
+        }
+    }
+}
---
+++ Tastee/Models/DTO/Users.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Tastee.Shared;
+
+namespace Tastee.Models.DTO
+{
+    public partial class User : SqlDataContext.Users
+    {
+        public PasswordVerificationResult VerifyPassword(LoginModel login)
+        {
+            var passwordHasher = new PasswordHasher<LoginModel>();
+            var verifyResult = passwordHasher.VerifyHashedPassword(login, PasswordHash, login.Password);
+            return verifyResult;
+        }
+
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns>HashPassword</returns>
+        public string SetPassword(string password)
+        {
+            var passwordHasher = new PasswordHasher<User>();
+            return passwordHasher.HashPassword(this, password);
+        }
+    }
+}
